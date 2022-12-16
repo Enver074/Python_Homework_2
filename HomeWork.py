867d4643df1e5c45bba962d44b3050e11c2d7535
Enver Ismailov <113299648+Enver074@users.noreply.github.com>
2022-12-16 18:15:59 +0300

    Первое задание второй вариант

--- HomeWork.py
+++ HomeWork.py
@@ -6,16 +6,23 @@
 # - 0,56 -> 11
 
 
-a = float(input("Введите число: "))
+# a = float(input("Введите число: "))
 
 
-while a != int(a):
-    a *= 10 
+# while a != int(a):
+#     a *= 10 
     
-sum = 0
-while a > 0:
-    sum += a % 10
-    a //= 10     
+# sum = 0
+# while a > 0:
+#     sum += a % 10
+#     a //= 10     
 
-print(int(sum))
+# print(int(sum))
 
+a = input("Введите число: ")
+sum = 0 
+for i in a:
+    if i.isdigit():
+        sum += int(i)
+
+print(sum)
